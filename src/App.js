6a0c7d9f44894def640cf1c076059d1b72e31afd
anastminsk <anast-minsk@yandex.ru>
2018-08-22 00:25:20 +0300

    feature: add theme switcher component

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ThemeSwitcher from './ThemeSwitcher';
+
+class App extends Component {
+  render() {
+    return <ThemeSwitcher />;
+  }
+}
+
+export default App;
